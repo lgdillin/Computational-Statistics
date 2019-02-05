77cc51e046cea0384d0a94b0b002663a9b8aaf5e
lgdillin <mailgd9@gmail.com>
2019-02-05 12:38:40 -0600

    update

--- Homework1.R
+++ Homework1.R
@@ -13,3 +13,32 @@ x[lapply(x, "%%", 2) == 0]
 # Remove the even elements, and print the array
 x = x[!x %in% x[lapply(x, "%%", 2) == 0]]
 x
+
+### Problem 2:
+func = function(n, mode) {
+  
+  if(mode == 1) {
+    result = 0
+    for(i in 1:n) {
+      result = result + 0.5 ^ i
+    }
+    return(result)
+  } else if(mode == 2) {
+    i = 1
+    result = 0
+    while(i <= n) {
+      result = result + 0.5 ^ i
+      i = i + 1
+    }
+    return(result)
+  } else {
+    ### NOT FINISHED
+  }
+
+  
+
+}
+
+func(2, 2)
+
+### Problem 3:
