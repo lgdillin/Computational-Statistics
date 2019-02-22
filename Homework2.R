ec6e1c44e19b64475391e481e589107542be824f
Liam Dillingham <mailgd9@gmail.com>
2019-02-22 12:40:56 -0600

    bit

--- Homework2.R
+++ Homework2.R
@@ -67,14 +67,39 @@ S_n = (1/n) * (t(m1) %*% m1)
 ev = eigen(S_n)$values
 Y = n * min(ev)
 
-hist(rgamma(1000, shape = 1, scale = 0.001), breaks = 30)
+hist(rtrunc(1000, spec = 'norm', a=0, b=2), breaks = 30)
+
+# hist(rexp(1000, rate = 0.000001), breaks = 30)
+####
 target = function(y) { ((1+sqrt(y)) / (2 * sqrt(y))) * exp(-1 * (y/2 + sqrt(y))) }
-    # integrate(function(y)  ((1+sqrt(y)) / (2*sqrt(y))) * exp(-(y/2 + sqrt(y))), 0, Inf)$value }
-curve(target,lwd=2,add=T,from = 0)
-alpha = 0.000001
-target1 = function(y) {
-  y^(alpha) / y^(alpha + 1)
+
+arfunc = function(M) {
+  while(TRUE) {
+    # value between [0,1]
+    u = runif(1)
+    
+    # value from envelope dist
+    x = rtrunc(1, spec = 'norm', a=0, b=2)
+    if(u <= target(x) / dtrunc(x, spec = 'norm', a=0, b=2) * M) {
+      return(x)
+    }
+  }
 }
-curve(target1, lwd=3, add=T)
+
+n = 10000
+samples = rep(0,n)
+for(i in 1:n) {
+  samples[i] = arfunc(0.05)
+}
+
+plot(samples)
+hist(samples, freq = F, breaks = 30)
+lines(seq(0, 2, 0.1), target(seq(0, 2, 0.1)),  col="red")
+
+N = (1/length(samples))
+sum = 0
+for(i in 1:length(samples)) {
+  sum = sum + log(samples[i])
+}
 
 
