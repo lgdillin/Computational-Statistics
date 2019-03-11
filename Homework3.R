044ce9bbb945ed9d346c2db90164ffcdd77a11a2
Liam Dillingham <mailgd9@gmail.com>
2019-03-11 11:41:22 -0500

    finished

--- Homework3.R
+++ Homework3.R
@@ -2,13 +2,13 @@
 #' @param n number of vehicles
 #' @param s number of bicycles
 #' @param theta proportion of bicycle traffic
-n <- 74
+n <- 70
 s_init <- 16
 theta_init <- s_init/n
 burn = 1000 # burn-in
 nmc = 1000 # number of Monte Carlo iterations
 
-gibbs <- function(s_init, theta_init, burn = 1000, nmc = 1000, alpha_0 = 2.0, beta_0 = 6.4){
+gibbs <- function(s_init, theta_init, burn = 1000, nmc = 2000, alpha_0 = 2.0, beta_0 = 6.4){
   
   theta <- rep(0, nmc+burn)
   s <- rep(0, nmc+burn)
@@ -30,21 +30,20 @@ plot(mcmc.fit$s[1001:2000], type = "l", main="s(i) samples")
 hist(mcmc.fit$s[1001:2000], breaks = 30, main="s(i) samples histogram", freq = F)
 acf(mcmc.fit$theta[1001:2000])
 mean(mcmc.fit$theta[1001:2000])
-median(mcmc.fit$theta[1001:2000])
+median(mcmc.fit$theta[1001:3000])
 
 ####################################
 ####################################
 ####################################
 ##### PRoblem 2
 lambda = 64
-# n <- 74 
-n_init = rpois(1, lambda) # Generate a random initial poisson value
+n_init = 78
 s_init <- 16
 theta_init <- s_init/n_init
 burn = 1000 # burn-in
 nmc = 1000 # number of Monte Carlo iterations
 
-gibbs <- function(s_init, theta_init, n_init, burn = 1000, nmc = 1000, alpha_0 = 2.0, beta_0 = 6.4){
+gibbs <- function(s_init, theta_init, n_init, burn = 1000, nmc = 3000, alpha_0 = 2.0, beta_0 = 6.4){
   
   n = rep(0, nmc+burn)
   theta <- rep(0, nmc+burn)
@@ -53,7 +52,7 @@ gibbs <- function(s_init, theta_init, n_init, burn = 1000, nmc = 1000, alpha_0 =
   s[1] = s_init; theta[1] = theta_init; n[1] = n_init;
   
   for (i in 2:(burn+nmc)) {
-    n[i] = rpois(1, lambda)
+    n[i] = rpois(1, lambda * (1-theta[i-1]))
     s[i] <- rbinom(1, n[i], theta[i-1])
     theta[i] <- rbeta(1, alpha_0 + s[i], beta_0 + n[i]-s[i])
   }
@@ -67,4 +66,5 @@ plot(mcmc.fit$s[1001:2000], type = "l")
 plot(mcmc.fit$n[1001:2000], type = "l")
 acf(mcmc.fit$theta[1001:2000])
 mean(mcmc.fit$theta[1001:2000])
-median(mcmc.fit$theta[1001:2000])
+median(mcmc.fit$theta[1001:4000])
+hist(mcmc.fit$s[1001:4000], breaks = 30, main="s(i) samples histogram", freq = F)
